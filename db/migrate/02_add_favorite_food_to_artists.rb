42ca3d51801e53ab3ff520e32857853eb77188d5
Eric Johnson <ericjohnson0216@icloud.com>
2019-10-06 00:02:05 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -2,7 +2,5 @@ class AddFavoriteFoodToArtists < ActiveRecord::Migration[4.2]
   def change
     add_column :artists, :favorite_food, :string
   end
-  def down
-    delete :favorite_food
-  end
+
 end
