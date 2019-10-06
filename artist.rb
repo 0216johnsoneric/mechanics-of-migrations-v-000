815d56828f4f731550f3d04e7850613dc0b0f04b
Eric Johnson <ericjohnson0216@icloud.com>
2019-10-06 00:02:37 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -2,9 +2,5 @@ class Artist < ActiveRecord::Base
   # Artist.create(name: 'Kelly', )
   # artist
   # Artist.new
-  def down
-    change_table :artists do |t|
-      t.remove :favorite_food
-    end
-  end
+  
 end
